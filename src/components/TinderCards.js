2fdf78a0c184dab9a566f0658c7f1e7f6ea211c5
fluturecode <fluturecode@gmail.com>
2020-09-14 18:02:53 -0400

    added Backend

--- src/components/TinderCards.js
+++ src/components/TinderCards.js
@@ -1,29 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TinderCard from "react-tinder-card";
 import "./TinderCards.css";
+import axios from "../axios";
 
 function Card() {
-	const [people, setPeople] = useState([
-		{
-			name: "Qazi",
-			url: "https://avatars0.githubusercontent.com/u/8461930?s=400&v=4",
-			language: "Python",
-			git: "https://github.com/CleverProgrammer",
-			forHire: "no",
-			relationStatus: "Committed",
-			sex: "male",
-		},
-		{
-			name: "Sonny",
-			url:
-				"https://avatars2.githubusercontent.com/u/24712956?s=460&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4",
-			language: "JavaScript",
-			git: "https://github.com/sonnysangha",
-			forHire: "yes",
-			relationStatus: "Committed",
-			sex: "male",
-		},
-	]);
+	const [people, setPeople] = useState([]);
+
+	useEffect(() => {
+		async function fetchData() {
+			const req = await axios.get("/people");
+			setPeople(req.data);
+		}
+		fetchData();
+	}, []);
 
 	return (
 		<div className="tinderCards__cardContainer">
